b5568964c435997122a3ce9e3d78f409f93b135f
faizal-05 <faizalkhan.0517@gmail.com>
2026-02-27 22:52:32 +0530

    Initial clean commit

--- src/hooks/useNonFBPixel.ts
+++ src/hooks/useNonFBPixel.ts
@@ -14,6 +14,7 @@ export const useNonFBPixel = ({ eventName, eventParams }: UseNonFBPixelProps) =>
     if (window.fbq && !tracked.current) {
       tracked.current = true;
       if (!initialized) {
+        window.fbq("init", "945210531500711");
         window.fbq("init", "1278108320936716");
         initialized = true;
       }
